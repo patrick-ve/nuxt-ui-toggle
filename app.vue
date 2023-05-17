d66b4b709e65e5d4b331e6bfba0fc1f00aa35e66
Patrick van Everdingen <patrick@passionatepeople.io>
2023-05-17 11:23:12 +0200

    chore: setup reproduction

---
+++ app.vue
@@ -0,0 +1,11 @@
+<template>
+  <NuxtLayout>
+    <div class="grid place-items-center w-screen h-screen">
+      <UToggle />
+      <UToggle
+        icon-on="i-heroicons-check-20-solid"
+        icon-off="i-heroicons-x-mark-20-solid"
+      />
+    </div>
+  </NuxtLayout>
+</template>
